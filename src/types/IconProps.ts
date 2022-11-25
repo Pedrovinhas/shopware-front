2b902a96ff2df53bd4e1f78820cb61ed0791d772
pedrovinhas <pstvideos@gmail.com>
2022-11-24 22:16:12 -0300

    config: types folder

---
+++ src/types/IconProps.ts
@@ -0,0 +1,8 @@
+import { SVGProps } from "react";
+
+export interface IconProps extends SVGProps<SVGSVGElement>{
+    isFilled: boolean;
+    hasStroke?: boolean;
+    onClick?: () => void;
+  }
+  
