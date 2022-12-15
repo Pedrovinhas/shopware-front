7b20d7692631ce3c845663066391d0029b40903b
pedrovinhas <pstvideos@gmail.com>
2022-12-15 18:08:10 -0300

    add: cart provider

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,20 @@ import { GlobalStyle } from './styles/global';
 import { theme } from './styles/theme'
 import { Router } from './routes/routes';
 import { CartContextProvider } from './contexts/CartContext';
+import { AuthContextProvider } from './contexts/AuthContext';
 
 export function App() {
 
 
   return (
+
     <CartContextProvider>
     <ThemeProvider theme={theme}>
       <GlobalStyle/>
       <Router/>
     </ThemeProvider>
     </CartContextProvider>
+
   )
 }
 
